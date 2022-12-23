21a804acb053d7894918acc202b35b46a2b536ef
Gleb Son <songleb81@gmail.com>
2022-12-23 09:20:05 +0200

    Update TextWordController.java

--- src/main/java/com/example/demo/controller/TextWordController.java
+++ src/main/java/com/example/demo/controller/TextWordController.java
@@ -1,11 +1,11 @@
 package com.example.demo.controller;
 
-import com.example.demo.Demo1Application;
 import com.example.demo.model.TextWordModel;
 import com.example.demo.service.TextWordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,8 +16,6 @@ public class TextWordController {
     @Autowired
     private TextWordService textWordService;
 
-    @Autowired
-    private Demo1Application palindromatic;
 
     @RequestMapping(method = RequestMethod.POST)
     @PostMapping("/textWord/addTextWord")
@@ -28,8 +26,10 @@ public class TextWordController {
     @RequestMapping(method= RequestMethod.DELETE)
     @DeleteMapping("/textWord/deleteTextWord/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteTextWord(@PathVariable String id) {
+    public ResponseEntity< String > deleteTextWord(@PathVariable String id) {
         textWordService.deleteTextWord(id);
+      return ResponseEntity.ok().body("Deleted successfully...!");
+
     }
 
     @RequestMapping(method = RequestMethod.PUT)
@@ -49,8 +49,7 @@ public class TextWordController {
     }
 
     @RequestMapping("/textWord/{palindrome}")
-    public String check(@PathVariable String palindrome) {
-        return String.valueOf(palindromatic.check(palindrome));
-
+    public boolean isPalindrome(@PathVariable String palindrome) {
+      return textWordService.isPalindrome(palindrome);
     }
 }
